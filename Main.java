1b6958edccd293facc4160b5dc74995f6837155d
vamsi232001 <vamsikundetiv@gmail.com>
2024-10-17 10:36:01 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main
   {
     public static void main(String args[])
       {
-      System.out.println("Hello world vamsiii");
+      System.out.println("Hello world vamsi");
+         System.out.println("Hello world vamsi");
      }
   }
